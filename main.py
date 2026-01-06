22ca64692aeb27cff50e909bc2021a4c596e8cec
Timofey Kiselev <timofey42k@gmail.com>
2026-01-06 14:42:11 +0300

    Почти готово локально

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import arcade
 #Задачи:
 #Написать локально для 2 игроков
 #Разобраться с физикой
-#Нарисовать текстуры
 
 # Задаём размер окна
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 500
 SCREEN_TITLE = "Пин Понг!"
 
 class MyGame(arcade.Window):
@@ -18,16 +17,16 @@ class MyGame(arcade.Window):
     def setup(self):
         # Создаём игрока
         self.ball = arcade.Sprite(":resources:images/pinball/pool_cue_ball.png", 0.5)
-        self.player1 = arcade.Sprite(":resources:images/pinball/pool_cue_ball.png", 0.5)
-        self.player2 = arcade.Sprite(":resources:images/pinball/pool_cue_ball.png", 0.5)
-        self.player1.center_x = 200
-        self.player1.center_y = 200
-        self.player2.center_x = 300
-        self.player2.center_y = 200
+        self.player1 = arcade.Sprite("pong.png", 0.5)
+        self.player2 = arcade.Sprite("pong.png", 0.5)
+        self.player1.center_x = 100
+        self.player1.center_y = 250
+        self.player2.center_x = 900
+        self.player2.center_y = 250
         self.ball.center_x = 200
         self.ball.center_y = 300
-        self.ball.change_x = 3
-        self.ball.change_y = -4
+        self.ball.change_x = 4
+        self.ball.change_y = -2
         
         self.player_list = arcade.SpriteList()
         self.balll = arcade.SpriteList()
@@ -37,10 +36,16 @@ class MyGame(arcade.Window):
         
     def on_update(self, delta_time):
         self.ball.update()
+        self.player1.update()
+        self.player2.update()
 
         if self.ball.top > SCREEN_HEIGHT or self.ball.bottom < 0:
+            self.ball.change_x *= 1.2
             self.ball.change_y *= -1
         if self.ball.right > SCREEN_WIDTH or self.ball.left < 0:
+            self.restart()
+            
+        if arcade.check_for_collision_with_list(self.ball, self.player_list):
             self.ball.change_x *= -1
 
     def on_draw(self): 
@@ -48,7 +53,37 @@ class MyGame(arcade.Window):
         self.clear()
         self.player_list.draw()
         self.balll.draw()
-
+    
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.S:
+            self.player1.change_y = -3
+        elif key == arcade.key.W:
+            self.player1.change_y = 3
+        if key == arcade.key.DOWN:
+            self.player2.change_y = -3
+        elif key == arcade.key.UP:
+            self.player2.change_y = 3
+        
+    def on_key_release(self, key, modifiers):
+        if key == arcade.key.S:
+            self.player1.change_y = 0
+        if key == arcade.key.W:
+            self.player1.change_y = 0
+        if key == arcade.key.DOWN:
+            self.player2.change_y = 0
+        if key == arcade.key.UP:
+            self.player2.change_y = 0
+    
+    def restart(self):
+        self.player1.center_x = 100
+        self.player1.center_y = 250
+        self.player2.center_x = 900
+        self.player2.center_y = 250
+        self.ball.center_x = 200
+        self.ball.center_y = 300
+        self.ball.change_x = 4
+        self.ball.change_y = -2
+        
 def main():
     game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     game.setup()
